c57573243f5193fbae8c6ae8436c5c1b2ba4fd63
Mason <liujianfeng@kingdee.com>
2019-11-28 12:44:29 +0800

    完成typewriting基本效果

--- src/views/Library/TypeWriting/index.js
+++ src/views/Library/TypeWriting/index.js
@@ -1,21 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TypeWrting from './typeWriting'
+import { Input, Button } from 'antd'
 import './index.less'
 
-const data = '改好了管理费很厉害家咯而过欧福莱不认可护理记录'
+const data = '现在开始是晚了点，但是总比明天早点......'
 
 const TypeArea = () => {
 
-    const areaRef = React.createRef()
+    const headRef = React.createRef()
+    const testRef = React.createRef()
+    const [text, setText] = useState('')
+    const [speed, setSpeed] = useState(200)
+    const [type, setType] = useState('text')
 
     React.useEffect(() => {
-        let res = new TypeWrting({ content: areaRef.current, data })
+        let res = new TypeWrting({ content: headRef.current, data, })
         res.writing()
-    }, [areaRef])
+    }, [headRef])
 
-    console.log(111)
+    const writeClick = () => {
+        testRef.current.innerHTML = ''
+        let res = new TypeWrting({ content: testRef.current, data: text, speed, type })
+        res.writing()
+    }
+
+    const inputChange = (e) => {
+        e.persist()
+        setText(e.target.value)
+    }
+
+    const speedChange = e => {
+        e.persist()
+        setSpeed(e.targe.value)
+    }
+
     return (
-        <div ref={areaRef} className="text-area"></div>
+        <div className="text-area">
+            <div ref={headRef}></div>
+            <Input value={text} onChange={e => inputChange(e)} placeholder="请输入一段文本~" />
+            <div className="text-speed">
+                <Button onClick={() => setSpeed(50)}>极快</Button>
+                <Button onClick={() => setSpeed(200)}>快</Button>
+                <Button onClick={() => setSpeed(500)}>慢</Button>
+                <Input onChange={e => speedChange(e)} placeholder="请输入打字速度（单位: ms）" />
+            </div>
+            <div className="text-animation">
+                <Button onClick={() => setType('pinyin')}>拼音（要求汉字）</Button>
+                <Button onClick={() => setType('animation')}>动画</Button>
+                <Button onClick={() => setType('leftAnimation')}>左动画</Button>
+                <Button onClick={() => setType('bottomAnimation')}>下动画</Button>
+            </div>
+            <Button onClick={writeClick}>查看打字效果</Button>
+            <div className="text-view" ref={testRef}></div>
+
+        </div>
+
     )
 }
 
--- src/views/Library/TypeWriting/typeWriting.js
+++ src/views/Library/TypeWriting/typeWriting.js
@@ -2,19 +2,37 @@ import './typeWriting.less'
 import Pinyin from 'pinyin'
 
 class TypeWriting {
-    constructor({ content, data }) {
+    constructor({ content, data, type = 'text', speed = 200 }) {
         content.classList.add('cursorPoint')
         this.content = content
         this.data = data
+        this.type = type
+        this.speed = speed
+
     }
     writing (index = 0) {
         let data = this.data
         let pinyinData = Pinyin(data)
-        // console.log(Pinyin(data))
         let arr = data.split('')
         if (index < arr.length) {
-            this.content.innerHTML += `${arr[index]}<sup>(${pinyinData[index]})</sup>`
-            setTimeout(this.writing.bind(this), 200, ++index)
+            switch (this.type) {
+                case 'pinyin':
+                    this.content.innerHTML += `${arr[index]}<sup>(${pinyinData[index]})</sup>`
+                    break
+                case 'animation':
+                    this.content.innerHTML = this.content.innerHTML.replace(/span-animation/g, '') + `<span class="span-animation">${arr[index]}</span>`
+                    break
+                case 'leftAnimation':
+                    this.content.innerHTML = this.content.innerHTML.replace(/span-animationleft/g, '') + `<span class="span-animationleft">${arr[index]}</span>`
+                    break
+                case 'bottomAnimation':
+                    this.content.innerHTML = this.content.innerHTML.replace(/span-animationbottom/g, '') + `<span class="span-animationbottom">${arr[index]}</span>`
+                    break
+                default:
+                    this.content.innerHTML += `${arr[index]}`
+            }
+
+            setTimeout(this.writing.bind(this), this.speed, ++index)
         }
     }
 }
