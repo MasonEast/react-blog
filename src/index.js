617186ad45363bfcb84aecf930fa6b7cebcc601d
Mason <liujianfeng@kingdee.com>
2020-01-13 11:46:58 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import dva from 'dva';
 import { createBrowserHistory as createHistory } from 'history';
-// import 'normalize.css/normalize.css' // a modern alternative to CSS resets
-// import '../public/init.css'
 const app = dva({
     history: createHistory()
 });
