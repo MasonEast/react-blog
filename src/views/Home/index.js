d04bed0cf28b3ecfc1f42d44ac5575110c1dff50
Mason <liujianfeng@kingdee.com>
2020-01-21 09:42:08 +0800

    给首页加loading

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,80 +1,93 @@
+/*
+ * @Description: 
+ * @Date: 2019-11-01 14:04:22
+ * @Author: mason
+ */
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import './index.less'
-import { Tag } from 'antd'
+import { Tag, Spin, Icon } from 'antd'
 import { Link } from 'dva/router';
 import { dateChange, request, translateMarkdown, judgeWidth } from '@/utils'
 import { requestURL } from '@/config'
+
+
+const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
+
 class Home extends Component {
 
     state = {
-        tags: []
+        tags: [],
+        spinFlag: true
     }
     componentDidMount () {
         this.props.getBlogs({ url: requestURL.blog })
         request({ url: requestURL.tags }).then(res => {
             let tags = res.data.map(item => ({ tag: item.tag, number: item.number }))
             this.setState({
-                tags
+                tags,
+                spinFlag: false
             })
         })
     }
 
     render () {
         const { blogs } = this.props.app
-        const { tags } = this.state
+        const { tags, spinFlag } = this.state
         return (
-            <div className="home-box">
-                <div className="home-box-top"></div>
-                <div className="home-box-left">
-                    {
-                        blogs.length && blogs.filter(item => item.status === 0).map(v => {
-                            return (
-                                <Link key={v._id} to={{
-                                    pathname: `/blog/${v._id}`,
-                                }}>
-                                    <section className="me-section">
-                                        <h2 className="me-title">{v.title}</h2>
-                                        <content className="me-detail" dangerouslySetInnerHTML={{ __html: translateMarkdown(v.content) }}>
-                                            {/* {v.content} */}
-                                        </content>
-                                        {judgeWidth() && <p className="me-submitTime">posted by {v.author} on {dateChange(v.date)}</p>}
-                                    </section>
-                                </Link>
-                            )
-                        })
-                    }
-                </div>
-                {judgeWidth() && <div className="home-box-right">
-                    <h4 style={{ marginBottom: 16 }}>MY TAGS:</h4>
-                    <div>
+            <Spin tip="我们正在努力加载哦" indicator={antIcon} spinning={spinFlag}>
+                <div className="home-box">
+                    <div className="home-box-top"></div>
+                    <div className="home-box-left">
                         {
-                            tags.map(v => {
-                                //实现随机颜色
+                            blogs.length > 0 && blogs.filter(item => item.status === 0).map(v => {
                                 return (
-                                    <Link
-                                        key={v.tag}
-                                        to={{
-                                            pathname: `/tags/${v.tag}`,
-                                            query: v.tag
-                                        }}
-                                    >
-                                        <Tag
-                                            className="home-box-right-tag"
-                                            onClick={this.blogsForTags}
-                                            color={'#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6)}
-                                        >
-                                            {v.tag}({v.number})
-                                       </Tag>
+                                    <Link key={v._id} to={{
+                                        pathname: `/blog/${v._id}`,
+                                    }}>
+                                        <section className="me-section">
+                                            <h2 className="me-title">{v.title}</h2>
+                                            <content className="me-detail" dangerouslySetInnerHTML={{ __html: translateMarkdown(v.content) }}>
+                                                {/* {v.content} */}
+                                            </content>
+                                            {judgeWidth() && <p className="me-submitTime">posted by {v.author} on {dateChange(v.date)}</p>}
+                                        </section>
                                     </Link>
                                 )
                             })
                         }
-
                     </div>
-                </div>}
-            </div>
+                    {judgeWidth() && <div className="home-box-right">
+                        <h4 style={{ marginBottom: 16 }}>MY TAGS:</h4>
+                        <div>
+                            {
+                                tags.map(v => {
+                                    //实现随机颜色
+                                    return (
+                                        <Link
+                                            key={v.tag}
+                                            to={{
+                                                pathname: `/tags/${v.tag}`,
+                                                query: v.tag
+                                            }}
+                                        >
+                                            <Tag
+                                                className="home-box-right-tag"
+                                                onClick={this.blogsForTags}
+                                                color={'#' + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).slice(-6)}
+                                            >
+                                                {v.tag}({v.number})
+                                       </Tag>
+                                        </Link>
+                                    )
+                                })
+                            }
 
+                        </div>
+                    </div>}
+                </div>
+
+            </Spin>
         );
     }
 }
